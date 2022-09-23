2cd87a74b247a62806987cada7fbdbaf22286f29
pinkros <arvid.leimar@iths.se>
2022-09-23 16:40:07 +0200

    itty bitty commitieh commity

--- Labb2/Butiken/Produkt.cs
+++ Labb2/Butiken/Produkt.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Butiken
 {
     public abstract class Produkt
     {
+        private double _pris;
+        public double Pris
+        {
+            get { return _pris; }
+            set { _pris = value; }
+        }
+
+        
+
+        private string _enhet;
+        private string _name;
+
+        private double _maengd;
+
+        public double Maengd
+        {
+            get { return _maengd; }
+            set { _maengd = value; }
+        }
+
+        public abstract void LaeggIVagn();
+
     }
 }
